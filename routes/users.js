de50ab877623824dfc2de23a0201da77c6664e78
Linc <gollink@gmail.com>
2015-10-17 13:02:57 +0800

    add upload code

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,23 @@
 var express = require('express');
 var router = express.Router();
-
+var multiparty = require('multiparty');
+var parseXlsx = require('excel');
+ var fs = require("fs") ;
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.post('/upload',function(req, res, next) {
+	var form = new multiparty.Form();
+	form.parse(req, function(err, fields, files) {
+		fs.writeFile("temp.xlsx",files,function (err) {
+	     if (err) throw err ;
+	     console.log("File Saved !"); //文件被保存
+	 	}) ;
+      res.writeHead(200, {'content-type': 'text/plain'});
+      res.write('received upload:\n\n');
+      res.send();
+    });
+	
+
+  	// res.send('respond with a resource');
 });
 
 module.exports = router;
