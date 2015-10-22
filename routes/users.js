2d17177bea80ac7c2b40311c5eee9c8c184b0106
gaolingfeng <gaolingfeng@gaolingfengdeMacBook-Air.local>
2015-10-22 17:20:17 +0800

    file upload

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,24 @@
 var express = require('express');
 var router = express.Router();
+var util = require('util');
 var multiparty = require('multiparty');
 var parseXlsx = require('excel');
  var fs = require("fs") ;
 /* GET users listing. */
 router.post('/upload',function(req, res, next) {
-	var form = new multiparty.Form();
+	var form = new multiparty.Form({
+		autoFiles:true,
+		uploadDir:'./temp'
+	});
 	form.parse(req, function(err, fields, files) {
-		fs.writeFile("temp.xlsx",files,function (err) {
-	     if (err) throw err ;
-	     console.log("File Saved !"); //文件被保存
-	 	}) ;
-      res.writeHead(200, {'content-type': 'text/plain'});
+		parseXlsx('./'+files.members[0].path, function(err, data) {
+		  if(err) throw err;
+		  console.log(data.length);
+		    // data is an array of arrays
+		});
+	  res.writeHead(200, {'content-type': 'text/plain'});
       res.write('received upload:\n\n');
-      res.send();
+      res.end(util.inspect({fields: fields, files: files}));
     });
 	
 
